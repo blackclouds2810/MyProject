405d7502f64eb1f82f58c811459fa50af2739e16
MightyZ <blackclouds2810@foxmail.com>
2022-06-08 11:27:14 +0800

    bugFix

---
+++ src/bugFix.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author: MightyZ
+ * @create: 2022/6/8
+ * @Description:
+ * @FileName: bugFix
+ */
+public class bugFix {
+    public static void main(String[] args) {
+        System.out.println("~~~bugFix~~~");
+    }
+}
